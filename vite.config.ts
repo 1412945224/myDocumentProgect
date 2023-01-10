4fb5cfe95ed0cc3b00776ec2bb62d517f3f833f8
1412945224 <15990097911@163.com>
2023-01-10 13:59:05 +0800

    chore：配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  // 设置别称
+  resolve: {
+    alias: [
+      {
+        find: "@", // 别名
+        replacement: resolve(__dirname, "src"), // 别名对应地址
+      },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "src/components"),
+      },
+      {
+        find: "@utils",
+        replacement: resolve(__dirname, "src/utils"),
+      },
+    ],
+    // 或者这种写法
+    // alias: {
+    //   "@": path.resolve(__dirname, "src"),
+    //   "@utils": path.resolve(__dirname, "src/utils"),
+    // },
+  },
+});
