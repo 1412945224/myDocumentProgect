13c6b2b0a92198b8e29389942c66193022c84ca6
1412945224 <15990097911@163.com>
2023-01-10 14:49:11 +0800

    chore:解决main.ts引入App.vue报错

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+/**
+ * @下面代码块 解决main.ts 引入./App.vue 报错
+ *
+ * @报错原因 未定义 .vue文件的类型，导致 ts 无法解析其类型，在env.d.ts中定义后即可解决。
+ */
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+
+  const vueComponent: DefineComponent<{}, {}, any>;
+
+  export default vueComponent;
+}
